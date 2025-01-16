676e3997d42b0a6511ecd6548f9a8dd54f878829
LeTanTru <letantru@gmail.com>
2025-01-16 18:08:35 +0700

    create Modal component

--- Todo App Drag - Drop React/src/components/Modal.jsx
+++
@@ -1,9 +0,0 @@
-import { createPortal } from 'react-dom';
-
-const Modal = () => {
-  return createPortal(
-    <div id='modal-container'>Modal</div>,
-    document.querySelector('body')
-  );
-};
-export default Modal;
--- Todo App Drag - Drop React/src/components/Toast.jsx
+++ Todo App Drag - Drop React/src/components/Toast.jsx
@@ -1,6 +1,39 @@
-import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
+const Toast = ({ title, message, type = 'info', duration = 3000, onClose }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, duration);
 
-const Toast = () => {
-  return createPortal(<div id='toast'></div>, document.querySelector('body'));
+    return () => clearTimeout(timer);
+  }, [duration, onClose]);
+
+  const icons = {
+    success: <i className='fa-solid fa-circle-check'></i>,
+    info: <i className='fa-solid fa-circle-info'></i>,
+    warning: <i className='fa-solid fa-triangle-exclamation'></i>,
+    error: <i className='fa-solid fa-circle-exclamation'></i>
+  };
+
+  return (
+    <div
+      className={`toast toast--${type}`}
+      style={{
+        animation: `slideInLeft 0.3s ease, fadeOut 1s ${
+          duration / 1000
+        }s forwards`
+      }}
+    >
+      <div className='toast__icon'>{icons[type]}</div>
+      <div className='toast__body'>
+        <h3 className='toast__title'>{title}</h3>
+        <p className='toast__message'>{message}</p>
+      </div>
+      <div className='toast__close' onClick={onClose}>
+        <i className='fa-solid fa-xmark'></i>
+      </div>
+    </div>
+  );
 };
+
 export default Toast;
